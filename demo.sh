e901a8fc2fcd87fe59d420c89b95074ccbb79ac8
Rayio2 <31609465+Rayio2@users.noreply.github.com>
2018-03-11 17:50:16 +0800

    Update demo.sh

--- demo.sh
+++ demo.sh
@@ -1,10 +1,13 @@
-mkdir $1
-cd $1
-mkdir css js
-echo ' <!DOCTYPE>
- <title>Hello</title>
- <h1>Hi</h1>' > index.html 
-echo 'h1{color: red;}' > css/style.css 
-echo ' var string = "Hello World"
- alert(string)' > js/main.js
-exit
+if [ -d $1 ]; then
+  echo 'error: file exists'
+  exit
+else
+  mkdir $1
+  cd $1
+  mkdir css js
+  echo "<!DOCTYPE>\n<title>Hello</title>\n<h1>Hi</h1>" > index.html
+  echo "h1{color: red;}" > css/style.css
+  echo "var string = \"Hello World\"\nalert(string)" > js/main.js
+  echo 'success'
+  exit
+fi
